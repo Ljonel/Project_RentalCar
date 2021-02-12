20994eedaa5401b8889181b68bdcc5a1e652f1fb
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2021-02-12 22:03:37 +0100

    create template for rent page

--- ProjectC-github/Wynajem.xaml.cs
+++ ProjectC-github/Wynajem.xaml.cs
@@ -23,5 +23,10 @@ namespace ProjectC_github
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
