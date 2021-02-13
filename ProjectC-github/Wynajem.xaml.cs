d7490f7014e229ed379fe90953aec9f42f91c41d
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2021-02-13 13:54:41 +0100

    show currently rented cars in rental page

--- ProjectC-github/Wynajem.xaml.cs
+++ ProjectC-github/Wynajem.xaml.cs
@@ -23,19 +23,25 @@ namespace ProjectC_github
         IList<string> nrRej = new List<string>();
         IList<int> employeeList= new List<int>();
         IList<int> clientList = new List<int>();
-       
+        /*    var nrQuery =
+                    (from item in _db.samochody select item.nr_rejestracyjny)
+                    .Except(from emp in _db.wynajem select emp.nr_rejestracyjny);
+        */
         public Wynajem()
         {
+            InitializeComponent();
+            ShowCombobox();
+            ShowRentalcar();
+        }
 
+        private void ShowCombobox()
+        {
             var nrQuery = from item in _db.rej_samochody select item;
             var employeeQuery = from item in _db.pracownicy select item;
             var clientsQuery = from item in _db.klienci select item;
-           
-           
-            InitializeComponent();
             foreach (var item in nrQuery)
             {
-               nrRej.Add(item.nr_rejestracyjny);
+                nrRej.Add(item.nr_rejestracyjny);
             }
             foreach (var item in employeeQuery)
             {
@@ -50,6 +56,32 @@ namespace ProjectC_github
             Pracownicy.ItemsSource = employeeList;
             Klienci.ItemsSource = clientList;
         }
+
+        private void ShowRentalcar()
+        {
+            /// <summary>
+            /// SELECT wynajem.nr_rejestracyjny, wynajem.id_wynajmu, samochody.marka 
+            /// FROM samochody 
+            /// JOIN wynajem ON wynajem.nr_rejestracyjny=samochody.nr_rejestracyjny;
+            /// </summary>
+                var rental = (from ep in _db.samochody
+                           join e in _db.wynajem
+                           on ep.nr_rejestracyjny
+                           equals e.nr_rejestracyjny
+                           select new
+                           {
+                               Id_wynaj = e.id_wynajmu,
+                               Nr_Rej = e.nr_rejestracyjny,
+                               Model = ep.model,
+                               Marka = ep.marka,
+                               Data_od = e.data_od,
+                               Data_do = e.data_do,
+                               Id_prac = e.id_pracownika,
+                               Id_kli = e.id_klienta
+                           });
+                tab_wynajem.ItemsSource = rental.ToList();
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
